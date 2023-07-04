fae25053a96a4df446354cd7d1c08fe0250dad6f
Agusti <6601142+agustif@users.noreply.github.com>
2023-07-04 18:51:31 +0200

    feat: manage discounts and uses them to total calculations of cart:

--- src/lib/Cart.svelte
+++ src/lib/Cart.svelte
@@ -1,7 +1,6 @@
 <script>
   import { cart } from '../stores/cart';
   import { formatLocalePrice } from '../utils/currency';
-
 </script>
 
 <section>
@@ -9,10 +8,21 @@
     {#each $cart.items as item (item.code)}
       <li>
         <span>{item.name} - {item.quantity} x {formatLocalePrice(item.price)}</span>
-        <button on:click={() => cart.deleteItem(item.code)}>Remove</button>
+        <button on:click={() => cart.removeItem(item.code)}>Remove</button>
+        <button on:click={() => cart.decreaseItem(item.code)}>Decrease</button>
       </li>
     {/each}
   </ul>
   <p>Total: {formatLocalePrice($cart.total)}</p>
+  {#if $cart.discounts.length > 0}
+    <h2>Discounts:</h2>
+    <ul>
+      {#each $cart.discounts as discount (discount.code)}
+        <li>
+          <span>Code: {discount.code} - Discount: {formatLocalePrice(discount.amount)}</span>
+        </li>
+      {/each}
+    </ul>
+  {/if}
   <button on:click={cart.clear}>Clear Cart</button>
 </section>
--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,56 +1,106 @@
 import { writable } from 'svelte/store';
 
-const initialState = {
-  items: [],
-  total: 0,
-};
-
 const createCart = () => {
-  const { subscribe, set, update } = writable(initialState);
+  const { subscribe, set, update } = writable({
+    items: [],
+    total: 0,
+    discounts: [],
+  });
 
-  const findItemIndex = (items, productCode) => {
-    return items.findIndex((item) => item.code === productCode);
-  }
+  const findItem = (state, productCode) => state.items.find((item) => item.code === productCode);
+  const findItemIndex = (state, productCode) => state.items.findIndex((item) => item.code === productCode);
 
+  const applyDiscounts = (state) => {
+    // Reset total and discounts
+    state.total = 0;
+    state.discounts = [];
+
+    state.items.forEach((item) => {
+      let discount = 0;
+      let total = item.price * item.quantity;
+
+      // Apply CAP discount
+      if (item.code === 'CAP' && item.quantity >= 3) {
+        const freeItems = Math.floor(item.quantity / 3);
+        discount = freeItems * item.price;
+      }
+
+      // Apply TSHIRT discount
+      if (item.code === 'TSHIRT' && item.quantity >= 3) {
+        discount = total * 0.25;
+      }
+
+      state.total += total - discount;
+      if (discount > 0) {
+        state.discounts.push({
+          code: item.code,
+          amount: discount,
+        });
+      }
+    });
+
+    return state;
+  };
+  // Adds one product item (stock/unit) to the cart
   const addItem = (product) => {
     update((state) => {
-      const index = findItemIndex(state.items, product.code);
+      let existingItem = findItem(state, product.code);
 
-      if (index !== -1) {
-        state.items[index].quantity += 1;
+      if (existingItem) {
+        existingItem.quantity += 1;
       } else {
         state.items.push({ ...product, quantity: 1 });
       }
 
-      state.total += product.price;
+      state = applyDiscounts(state);
       return state;
     });
-  }
+  };
 
-  const deleteItem = (productCode) => {
+  // Removes all added items from product code from the cart
+  const removeItem = (productCode) => {
     update((state) => {
-      const index = findItemIndex(state.items, productCode);
+      const itemIndex = findItemIndex(state, productCode);
 
-      if (index !== -1) {
-        const item = state.items[index];
-        state.total -= item.price * item.quantity;
-        state.items.splice(index, 1);
+      if (itemIndex !== -1) {
+        state.items.splice(itemIndex, 1);
       }
 
+      state = applyDiscounts(state);
       return state;
     });
-  }
+  };
 
-  function clear() {
-    set(initialState);
-  }
+  // Decreases by one the cart order of the product code
+  const decreaseItem = (productCode) => {
+    update((state) => {
+      let existingItem = findItem(state, productCode);
+
+      if (existingItem) {
+        existingItem.quantity -= 1;
+
+        if (existingItem.quantity === 0) {
+          const itemIndex = findItemIndex(state, productCode);
+          state.items.splice(itemIndex, 1);
+        }
+      }
+
+      state = applyDiscounts(state);
+      return state;
+    });
+  };
+
+  const clear = () => {
+    set({ items: [], total: 0, discounts: [] });
+  };
 
   return {
     subscribe,
     addItem,
-    deleteItem,
+    removeItem,
+    decreaseItem,
     clear,
   };
-}
+};
 
 export const cart = createCart();
