86c8f86b584b2b88e824491964a3524191130be4
Agusti <6601142+agustif@users.noreply.github.com>
2023-07-05 08:40:24 +0200

    feat: separate button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  export let disabled = false;
+  export let className = "";
+  export let onClick;
+</script>
+  
+<button class={className} disabled={disabled} on:click={onClick}> <slot /> </button>
+
+<style>
+  button {
+    padding: 0.75rem 1rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    background: #fff;
+    color: black;
+    cursor: pointer;
+    justify-self: center;
+    width: 100%;
+    font-size: 120%;
+    background-color: #6E51F6;
+    color: white;
+  }
+  button:hover {
+    background-color: #513da9;
+    color: white;
+    transition: all 0.3s ease-in-out;
+  }
+  button:disabled {
+    cursor: not-allowed;
+    background-color: #ccc;
+  }
+  .remove {
+    background-color: rgb(194, 0, 0);
+    max-width: 100px; 
+    font-size: 70%;
+  }
+  .remove:hover {
+    background-color: rgb(129, 0, 0);
+  }
+</style>
