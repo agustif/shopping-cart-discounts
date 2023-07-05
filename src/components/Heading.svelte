6de9a9611438588c5acf8b8c2d6f9d32d4cffaff
Agusti <6601142+agustif@users.noreply.github.com>
2023-07-05 08:39:16 +0200

    feat: new separate heading

---
+++ src/components/Heading.svelte
@@ -0,0 +1,16 @@
+<h2>
+  <slot />
+</h2>
+<hr />
+
+<style>
+  h2 {
+    font-size: 1.5rem;
+    font-weight: 400;
+    margin-bottom: 1rem;
+  }
+  hr {
+    border: 1px solid #ccc;
+    margin: 0;
+  }
+</style>
