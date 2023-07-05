bfa588ca8e6becf40735033b218412eb0be70dad
Agusti <6601142+agustif@users.noreply.github.com>
2023-07-05 08:41:21 +0200

    feat: separate checkout confirmation component

---
+++ src/components/CheckoutConfirmation.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Button from "./Button.svelte"
+  import { formatLocalePrice } from '../utils/currency';
+  import { cart } from '../stores/cart';
+</script>
+
+<div>
+  <hr />
+    <div class="total-container">
+      <h3>Cart total</h3>
+      <p>{formatLocalePrice($cart.total)}</p>
+    </div>
+  <Button onClick={
+  () => {
+    alert("Your order has been processed");
+    cart.clear();
+  }}> Confirm </Button>
+</div>
+
+<style>
+  .total-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
