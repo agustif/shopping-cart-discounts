b86c04a66daad59ecbbcc5238999ee9c6408599a
Agusti <6601142+agustif@users.noreply.github.com>
2023-07-05 08:41:01 +0200

    feat: separate cartItems into it's own component

---
+++ src/components/CartItems.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { cart } from '../stores/cart';
+  import { formatLocalePrice } from '../utils/currency';
+</script>
+  
+<ul class="cart-item-list">
+  {#each $cart.items as item (item.code)}
+    <li class="cart-item-detail">
+      <p>x{item.quantity} {item.name}</p>
+      <p>{formatLocalePrice(item.price)}</p>
+    </li>
+  {/each}
+</ul>
+
+<style>
+  .cart-item-list {
+    height: 100%;
+    align-items: start;
+    justify-content: flex-start;
+  }
+  .cart-item-detail {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 270px;
+    margin-bottom: 0;
+    padding-bottom: 0;
+    line-height: 1;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: -10px;
+  }
+</style>
